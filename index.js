8541be2fd47ee7e6588631125baf1e15900653f2
Lee Li (yul040) <moran.li@usask.ca>
2018-03-27 19:58:43 -0600

    add responce header

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ var app = express()
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(fileUpload());
 
 app.use('/image', express.static(path.join(__dirname, 'image')))
